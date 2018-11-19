ef21dd7e560bf89904ab8a80f51a60c42295aa70
Jim Bulkowski <jim.b@paperelectron.com>
2018-11-19 10:06:03 -0500

    Removes more old source files

--- src_off/newTaskRunners/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file index
- * @author Jim Bulkowski <jim.b@paperelectron.com>
- * @project exigency
- * @license MIT {@link http://opensource.org/licenses/MIT}
- */
-
-
-export {TaskRunner, TaskHandler} from "./TaskRunner"
